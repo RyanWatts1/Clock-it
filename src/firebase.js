35b04ec712c4fa9d7fa5a0e0fa4ce40d9340d304
RyanWatts1 <devshop202@yahoo.com>
2020-11-15 15:10:02 -0500

    Time Work

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore'
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyCguVTE8vhbZNcnJtI-tBqW_66sG_ld-Zg",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "just-clock-it-8f51c.firebaseapp.com",
     databaseURL: "https://just-clock-it-8f51c.firebaseio.com",
     projectId: "just-clock-it-8f51c",
     storageBucket: "just-clock-it-8f51c.appspot.com",
     messagingSenderId: "223346536356",
-    appId: "1:223346536356:web:0dd20c772e89d8e168db31",
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: "G-P4W4TNFQLS"
   };
   // Initialize Firebase
